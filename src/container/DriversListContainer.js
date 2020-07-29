f16e8631bf710d817881f94691f32fb752123773
Pankaj Bat <pankaj.bat@myntra.com>
2020-07-30 00:15:03 +0530

    chaged component location

--- src/container/DriversListContainer.js
+++ src/container/DriversListContainer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from "@material-ui/core/Typography";
 import DriversList from '../components/Drivers/DriversList';
-import AddNewDriver from '../components/Drivers/AddNewDriver';
+import AddNewDriver from './AddNewDriver';
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDriversList } from '../store/actions';
