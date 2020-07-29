c5cb75fee7f39cc6cc54bd97bf0ca48331f1b62c
Pankaj Bat <pankaj.bat@myntra.com>
2020-07-30 00:22:31 +0530

    add new driver file to container

---
+++ src/container/AddNewDriver.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import AddNewDriverForm from '../components/Drivers/AddNewDriverForm';
+import DriverDetailsSummary from '../components/Drivers/DriverDetailsSummary';
+
+const TITLE = { 1: 'Add New Driver', 2: 'Driver Summary' };
+
+const AddNewDriver = ({ handleClose }) => {
+  const [step, setStep] = useState(1);
+  const [driverDetails, setDriverDetails] = useState({});
+
+  const gotoSummary = (data) => { 
+    setDriverDetails(data);
+    setStep(2); 
+  }
+
+  const gotoForm = () => setStep(1);
+
+  const onSubmit = () => { console.log(driverDetails); }
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open>
+      <DialogTitle id="form-dialog-title">{TITLE[step]}</DialogTitle>
+      <DialogContent>
+        {step === 1 && <AddNewDriverForm data={driverDetails} save={gotoSummary} />}
+        {step === 2 && <DriverDetailsSummary details={driverDetails} onSubmit={onSubmit} onBack={gotoForm}/>}
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default AddNewDriver;
